6d196f8cbf8211038df0fe3b6efe38ea84e9ea3d
shinYuki0214 <sin2022web@gmail.com>
2024-07-30 20:17:05 +0900

    商品関連

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 class ItemSeeder extends Seeder
 {
     /**
@@ -15,5 +18,23 @@ class ItemSeeder extends Seeder
     public function run()
     {
         //
+
+        DB::table('items')->insert([
+            [
+                'name' => 'カット',
+                'memo' => 'カットの詳細',
+                'price' => 6000
+            ],
+            [
+                'name' => 'カラー',
+                'memo' => 'カラーの詳細',
+                'price' => 8000
+            ],
+            [
+                'name' => 'パーマ',
+                'memo' => 'パーマの詳細',
+                'price' => 13000
+            ],
+        ]);
     }
 }
